9807874b1437b90d594d4d6f2212e75d4ae2d6c7
FreydZig <zigmunt.freyd.95@gmail.com>
2022-09-28 17:27:05 +0700

    feat: tables of DataBase

---
+++ src/CM.TeamReportDB/dbo/Tables/Teams.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Teams] (
+    [TeamId]   INT           IDENTITY (1, 1) NOT NULL,
+    [TeamName] NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([TeamId] ASC),
+    CHECK (len([TeamName])<=(100))
+);
+
