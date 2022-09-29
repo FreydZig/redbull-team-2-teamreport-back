cc2ff5ab5d39d294dd8b523140b1541f1bdb49e6
FreydZig <zigmunt.freyd.95@gmail.com>
2022-09-29 20:46:39 +0700

    feat: leaderrepository

---
+++ src/CM.TeamRepots.DataLayer/Repositories/ReportsRepository.cs
@@ -0,0 +1,40 @@
+﻿using CM.TeamRepots.DataLayer.Entity;
+using CM.TeamRepots.DataLayer.Interfaces;
+
+namespace CM.TeamRepots.DataLayer.Repositories
+{
+    public class ReportsRepository : IRepository<Reports>
+    {
+        private readonly TRDbContext _context;
+
+        public ReportsRepository()
+        {
+            _context = new TRDbContext();
+        }
+
+        public void Create(Reports entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int entityCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Reports> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reports Read(int entityCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Reports entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
