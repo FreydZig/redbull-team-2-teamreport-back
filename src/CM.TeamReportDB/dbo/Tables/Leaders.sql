e0b15fdc304c0ce0369a6cea0099ecc16bb75e55
FreydZig <zigmunt.freyd.95@gmail.com>
2022-09-29 15:10:39 +0700

    feat: add leadres table

---
+++ src/CM.TeamReportDB/dbo/Tables/Leaders.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Leaders] (
+    [LiderId] INT IDENTITY (1, 1) NOT NULL,
+    [UserId]  INT NOT NULL,
+    [TeamId]  INT NOT NULL,
+    FOREIGN KEY ([TeamId]) REFERENCES [dbo].[Teams] ([TeamId]),
+    FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([UserId])
+);
+
