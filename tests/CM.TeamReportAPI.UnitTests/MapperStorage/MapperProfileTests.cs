88a2d4e2d821c572c021e978176eb98a887f4898
FreydZig <zigmunt.freyd.95@gmail.com>
2022-10-04 19:55:37 +0700

    fix: some fix

---
+++ tests/CM.TeamReportAPI.UnitTests/MapperStorage/MapperProfileTests.cs
@@ -0,0 +1,20 @@
+﻿using CM.TeamReportAPI.MapperStore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CM.TeamReportAPI.UnitTests.MapperStorage
+{
+    public class MapperProfileTests
+    {
+        [Fact]
+        public void ShouldBeAbleToCreateMapperProfile()
+        {
+            MapperProfile m = new MapperProfile();
+
+            Assert.NotNull(m);
+        }
+    }
+}
