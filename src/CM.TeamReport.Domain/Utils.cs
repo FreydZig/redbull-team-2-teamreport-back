65f70d7ca5189fc028e3f25722672a7d059c56d6
FreydZig <zigmunt.freyd.95@gmail.com>
2022-10-13 12:23:54 +0700

    fix: Leader controller and add Utils

---
+++ src/CM.TeamReport.Domain/Utils.cs
@@ -0,0 +1,20 @@
+﻿namespace CM.TeamReport.Domain
+{
+    public class Utils
+    {
+        public static int DayOfWeekToInt(DateTime date)
+        {
+            switch (date.DayOfWeek)
+            {
+                case DayOfWeek.Monday: return -1;
+                case DayOfWeek.Tuesday: return -2;
+                case DayOfWeek.Wednesday: return -3;
+                case DayOfWeek.Thursday: return -4;
+                case DayOfWeek.Friday: return -5;
+                case DayOfWeek.Saturday: return -6;
+                case DayOfWeek.Sunday: return -7;
+                default: return 0;
+            }
+        }
+    }
+}
